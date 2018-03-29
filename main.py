de98a1e8c0e8a48a4360af7aa810cc7cecb8671e
Chris Dryden <christopher.paul.dryden@gmail.com>
2018-03-29 19:06:33 -0400

    Updated ports configuration

--- main.py
+++ main.py
@@ -119,5 +119,5 @@ def static_proxy(path):
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True, host='127.0.0.1', port=5000)
-#    app.run(debug=True, host='0.0.0.0', port=80)
+#    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=80)
